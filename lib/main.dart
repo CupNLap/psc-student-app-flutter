3e7aafa61cc448620e43c70e0c304861ba0388f6
45afeef <45afeef@gmail.com>
2023-12-02 12:42:22 +0530

    Added cloud_firestore to pubspec, Enabled multidex Added dummy value to firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -10,6 +19,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // CollectionReference db = FirebaseFirestore.instance.collection("places");
+    CollectionReference<Map<String, dynamic>> placeCollection =
+        FirebaseFirestore.instance.collection('Places');
+
+    placeCollection.add({"first": "hai", "second": 23, "third": 3}).then(
+        (value) => {print('hai')});
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
