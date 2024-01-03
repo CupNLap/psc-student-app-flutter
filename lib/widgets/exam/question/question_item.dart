132ba3e4e009c78e0c73d7dcf3d30eab84b05d7f
45afeef <45afeef@gmail.com>
2024-01-03 11:21:31 +0530

    Added Logic to restrict the user selection for only once

--- lib/widgets/exam/question/question_item.dart
+++ lib/widgets/exam/question/question_item.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:student/model/question.dart';
 
@@ -9,11 +8,16 @@ class QuestionView extends StatefulWidget {
     this.showAnswers = false,
     this.selectedOption,
     this.onOptionSelected,
+    this.selectOptionsOnlyOnce = false,
     super.key,
   })  : assert(
           onOptionSelected == null ||
               (showAnswers == false && selectedOption == null),
           'showAnswers and selectedOptions should not be provided if onOptionSelected is provided',
+        ),
+        assert(
+          onOptionSelected != null || !selectOptionsOnlyOnce,
+          'selectOptionsOnlyOnce should only be provided if onOptionSelected is provided',
         ),
         assert(
           selectedOption == null || showAnswers == true,
@@ -25,12 +29,15 @@ class QuestionView extends StatefulWidget {
   final bool showAnswers;
   final String? selectedOption;
   final Function(String option)? onOptionSelected;
+  final bool selectOptionsOnlyOnce;
 
   @override
   State<QuestionView> createState() => _QuestionViewState();
 }
 
 class _QuestionViewState extends State<QuestionView> {
+  String? _currentSelectedOption;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -45,35 +52,51 @@ class _QuestionViewState extends State<QuestionView> {
           Text(widget.question.question),
           // Options
           for (int i = 0; i < widget.question.options.length; i++)
-            Card(
-              color: widget.question.answer == widget.question.options[i] &&
-                      widget.showAnswers
-                  ? Colors.green[50]
-                  : Colors.white,
-              clipBehavior: Clip.antiAlias,
-              child: Container(
-                width: double.maxFinite,
-                decoration:
-                    widget.question.answer == widget.question.options[i] &&
-                            widget.showAnswers
-                        ? const BoxDecoration(
-                            border: Border(
-                              left: BorderSide(
-                                color: Colors.green, // Customize the color here
-                                width: 10.0, // Adjust the width as needed
+            GestureDetector(
+              onTap: () {
+                if (!widget.selectOptionsOnlyOnce ||
+                    _currentSelectedOption == null) {
+                  setState(() {
+                    _currentSelectedOption = widget.question.options[i];
+                  });
+                  if (widget.onOptionSelected != null) {
+                    widget.onOptionSelected!(widget.question.options[i]);
+                  }
+                }
+              },
+              child: Card(
+                color: widget.question.answer == widget.question.options[i] &&
+                        widget.showAnswers
+                    ? Colors.green[50]
+                    : _currentSelectedOption == widget.question.options[i]
+                        ? Colors.blue[50] // Highlight selected option
+                        : Colors.white,
+                clipBehavior: Clip.antiAlias,
+                child: Container(
+                  width: double.maxFinite,
+                  decoration:
+                      widget.question.answer == widget.question.options[i] &&
+                              widget.showAnswers
+                          ? const BoxDecoration(
+                              border: Border(
+                                left: BorderSide(
+                                  color: Colors.green,
+                                  width: 10.0,
+                                ),
                               ),
-                            ),
-                          )
-                        : null,
-                padding: const EdgeInsets.all(12.0),
-                child: Text(
-                  widget.question.options[i],
-                  style: TextStyle(
-                      color: widget.showAnswers &&
-                              widget.question.options[i] ==
-                                  widget.question.answer
-                          ? Colors.green
-                          : Colors.grey),
+                            )
+                          : null,
+                  padding: const EdgeInsets.all(12.0),
+                  child: Text(
+                    widget.question.options[i],
+                    style: TextStyle(
+                      color: widget.showAnswers
+                          ? widget.question.options[i] == widget.question.answer
+                              ? Colors.green
+                              : Colors.grey
+                          : Colors.black,
+                    ),
+                  ),
                 ),
               ),
             ),
