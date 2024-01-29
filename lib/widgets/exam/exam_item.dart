c93544d865f82797172d5178bf9615038565d746
Afeef K K <45afeef@gmail.com>
2024-01-29 18:02:25 +0530

    🪲 UI overflow bug fixed

--- lib/widgets/exam/exam_item.dart
+++ lib/widgets/exam/exam_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:student/model/batch.dart';
 import 'package:student/pages/exam_screen.dart';
@@ -15,54 +16,47 @@ class ExamItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final DateTime dateTime = exam.startAt.toDate();
     final String formattedTime =
-        '${dateTime.day}/${dateTime.month}/${dateTime.year} ${dateTime.hour}:${dateTime.minute}';
+        DateFormat('hh:mm a MMM dd ').format(exam.startAt.toDate());
 
     Widget _buildContents() {
       return Padding(
         padding: const EdgeInsets.all(16.0),
-        child: IntrinsicHeight(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Text(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Text(
                     exam.name,
                     style: Theme.of(context).textTheme.titleLarge,
                   ),
-                  const Spacer(),
-                  const SizedBox(height: 16.0),
-                  Text(
-                    '${exam.time} minutes',
-                    style: Theme.of(context).textTheme.bodySmall,
-                  )
-                ],
-              ),
-              Image.asset(exam.icon, width: 50, height: 50),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Text(
-                    exam.code,
-                    style: Theme.of(context).textTheme.bodySmall,
-                  ),
-                  const Spacer(),
-                  Text(
-                    formattedTime,
-                    style: Theme.of(context).textTheme.bodyLarge,
-                  ),
-                ],
-              ),
-            ],
-          ),
+                ),
+                Text(
+                  exam.code,
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  formattedTime,
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                Center(child: Image.asset(exam.icon, width: 50, height: 50)),
+                Text(
+                  '${exam.time} minutes',
+                  style: Theme.of(context).textTheme.bodySmall,
+                )
+              ],
+            )
+          ],
         ),
       );
     }
@@ -73,52 +67,52 @@ class ExamItem extends StatelessWidget {
         borderRadius: BorderRadius.circular(8),
       ),
       child: InkWell(
-          onTap: disabled
-              ? null
-              : () {
-                  SharedPreferences.getInstance().then((pref) {
-                    bool isFirstAttempt =
-                        pref.getBool('${exam.ref?.path}') ?? true;
+        onTap: disabled
+            ? null
+            : () {
+                SharedPreferences.getInstance().then((pref) {
+                  bool isFirstAttempt =
+                      pref.getBool('${exam.ref?.path}') ?? true;
 
-                    if (isFirstAttempt) {
-                      showDialog(
-                        context: context,
-                        builder: (_) => AlertDialog(
-                          content: Text(
-                              'Are you ready to attempt the exam - "${exam.name}"?'),
-                          actions: [
-                            TextButton(
-                                onPressed: () => Navigator.pop(context),
-                                child: const Text("Cancel")),
-                            TextButton(
-                              onPressed: () {
-                                // Close the AlertDialog
-                                Navigator.pop(context);
-                                // Navigate to the exam
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => ExamScreen(
-                                      examRef: exam.ref,
-                                      time: exam.time,
-                                    ),
+                  if (isFirstAttempt) {
+                    showDialog(
+                      context: context,
+                      builder: (_) => AlertDialog(
+                        content: Text(
+                            'Are you ready to attempt the exam - "${exam.name}"?'),
+                        actions: [
+                          TextButton(
+                              onPressed: () => Navigator.pop(context),
+                              child: const Text("Cancel")),
+                          TextButton(
+                            onPressed: () {
+                              // Close the AlertDialog
+                              Navigator.pop(context);
+                              // Navigate to the exam
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => ExamScreen(
+                                    examRef: exam.ref,
+                                    time: exam.time,
                                   ),
-                                );
-                                pref.setBool('${exam.ref?.path}', false);
-                              },
-                              child: const Text("OK"),
-                            ),
-                          ],
-                        ),
-                      );
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                          content:
-                              Text('You have already Attempted this exam')));
-                    }
-                  });
-                },
-          child: _buildContents()),
+                                ),
+                              );
+                              pref.setBool('${exam.ref?.path}', false);
+                            },
+                            child: const Text("OK"),
+                          ),
+                        ],
+                      ),
+                    );
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        content: Text('You have already Attempted this exam')));
+                  }
+                });
+              },
+        child: _buildContents(),
+      ),
     );
   }
 }
