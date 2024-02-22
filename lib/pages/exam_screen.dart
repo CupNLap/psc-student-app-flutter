bd611d1e31aaee0ac6c61a1bd2b2b26c2e406f31
Afeef K K <45afeef@gmail.com>
2024-02-22 11:42:50 +0530

    🪲 Bug fix - fixed the issue of refreshing on back press by removing the restrict pop widget Converted to stateless widget

--- lib/pages/exam_screen.dart
+++ lib/pages/exam_screen.dart
@@ -10,10 +10,9 @@ import '../provider/exam_provider.dart';
 import '../widgets/exam/question/question_item.dart';
 import '../widgets/exam/timer.dart';
 import '../widgets/utils/gap.dart';
-import '../widgets/utils/restrict_pop.dart';
 import 'result_screen.dart';
 
-class ExamScreen extends StatefulWidget {
+class ExamScreen extends StatelessWidget {
   const ExamScreen({
     super.key,
     required this.examRef,
@@ -24,26 +23,75 @@ class ExamScreen extends StatefulWidget {
   final int? time;
 
   @override
-  State<ExamScreen> createState() => _ExamScreenState();
-}
-
-class _ExamScreenState extends State<ExamScreen> {
-  int currentQuestionIndex = 0;
-  late final ExamProvider _examProvider;
-
-  @override
-  void initState() {
-    super.initState();
+  Widget build(BuildContext context) {
     logPageName('exam_screen.dart');
+    late final ExamProvider _examProvider;
 
     _examProvider = Provider.of<ExamProvider>(context, listen: false);
-  }
 
-  @override
-  Widget build(BuildContext context) {
+    void _handleExamSubmit() {
+      _examProvider.examCompleted();
+      _examProvider.saveExamResultInFirestore();
+
+      Navigator.pop(context);
+
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => const ResultsScreen()));
+    }
+
+    CustomScrollView ExamView(Exam exam) {
+      return CustomScrollView(
+        physics: const BouncingScrollPhysics(),
+        slivers: [
+          SliverPersistentHeader(
+            pinned: true,
+            delegate: MyStickyHeaderDelegate(
+              minHeight: 150.0,
+              maxHeight: 250.0,
+              child: Container(
+                color: Colors.grey[200],
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(exam.name, style: const TextStyle(fontSize: 24.0)),
+                      TimerWidget(
+                          // Get the time from Parent widget, If there is no time available then set it to 75% of length of the exam as minutes
+                          minutes: time ?? exam.questions.length * 0.75 ~/ 1,
+                          onTimerOver: () {
+                            _handleExamSubmit();
+                          }),
+                      const GoogleBannerAd(),
+                      Text(exam.code, style: const TextStyle(fontSize: 12.0)),
+                    ]),
+              ),
+            ),
+          ),
+          SliverList(
+              delegate: SliverChildBuilderDelegate(
+                  (context, index) => QuestionView(
+                        exam.questions[index],
+                        title: 'Question - ${index + 1}',
+                        selectedOption:
+                            _examProvider.getSelectedOptionAtIndex(index),
+                        onOptionSelected: (option) {
+                          _examProvider.setTheSelectedOptionAtIndex(
+                              index, option);
+                        },
+                        selectOptionsOnlyOnce: true,
+                        // showAnswers: true,
+                      ),
+                  childCount: exam.questions.length)),
+          SliverToBoxAdapter(
+              child: ElevatedButton(
+                  onPressed: _handleExamSubmit,
+                  child: const Text("Submit the Exam"))),
+        ],
+      );
+    }
+
     return Scaffold(
       body: FutureBuilder<DocumentSnapshot<Exam>>(
-        future: widget.examRef
+        future: examRef
             .withConverter(
               fromFirestore: (snapshot, _) => Exam.fromFirestore(snapshot),
               toFirestore: (exam, _) => exam.toFirestore(),
@@ -78,81 +126,15 @@ class _ExamScreenState extends State<ExamScreen> {
           }
 
           // Record that the exam has started
-          _examProvider.examStarted(exam, widget.examRef.path);
+          _examProvider.examStarted(exam, examRef.path);
           SharedPreferences.getInstance().then((pref) {
-            pref.setBool('${widget.examRef.path}', false);
+            pref.setBool('${examRef.path}', false);
           });
-          
-          return RestrictPop(
-              title: "You can't leave exam before completion",
-              content:
-                  "Please make sure that you have completed the exam before leaving it.",
-              negativeActionText: "OK",
-              positiveActionText: "Continue",
-              child: SafeArea(child: ExamView(exam)));
+
+          return SafeArea(child: ExamView(exam));
         },
       ),
     );
-  }
-
-  CustomScrollView ExamView(Exam exam) {
-    return CustomScrollView(
-      physics: const BouncingScrollPhysics(),
-      slivers: [
-        SliverPersistentHeader(
-          pinned: true,
-          delegate: MyStickyHeaderDelegate(
-            minHeight: 150.0,
-            maxHeight: 250.0,
-            child: Container(
-              color: Colors.grey[200],
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(exam.name, style: const TextStyle(fontSize: 24.0)),
-                    TimerWidget(
-                        // Get the time from Parent widget, If there is no time available then set it to 75% of length of the exam as minutes
-                        minutes:
-                            widget.time ?? exam.questions.length * 0.75 ~/ 1,
-                        onTimerOver: () {
-                          _handleExamSubmit();
-                        }),
-                    const GoogleBannerAd(),
-                    Text(exam.code, style: const TextStyle(fontSize: 12.0)),
-                  ]),
-            ),
-          ),
-        ),
-        SliverList(
-            delegate: SliverChildBuilderDelegate(
-                (context, index) => QuestionView(
-                      exam.questions[index],
-                      title: 'Question - ${index + 1}',
-                      selectedOption:
-                          _examProvider.getSelectedOptionAtIndex(index),
-                      onOptionSelected: (option) {
-                        _examProvider.setTheSelectedOptionAtIndex(
-                            index, option);
-                      },
-                      selectOptionsOnlyOnce: true,
-                      // showAnswers: true,
-                    ),
-                childCount: exam.questions.length)),
-        SliverToBoxAdapter(
-            child: ElevatedButton(
-                onPressed: _handleExamSubmit,
-                child: const Text("Submit the Exam"))),
-      ],
-    );
-  }
-
-  void _handleExamSubmit() {
-    _examProvider.examCompleted();
-
-    Navigator.pop(context);
-
-    Navigator.push(context,
-        MaterialPageRoute(builder: (context) => const ResultsScreen()));
   }
 }
 
